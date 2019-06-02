bde52f994fc9836613c1665bc7a6f2622d8c54cd
t-kusakabe <ttmac0704@gmail.com>
2019-06-03 01:10:43 +0900

    AppBar white

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'startup name generator',
+        theme: ThemeData(
+            primaryColor: Colors.white,
+        ),
         home: RandomWords(),
     );
   }
